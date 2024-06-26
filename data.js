cef1b0afeffb38a4899bbc71e4318da5f4a737c6
emilyannemoses <e.a.moses@gmail.com>
2024-06-26 17:17:52 -0500

    adding names of poses to data file

--- data.js
+++ data.js
@@ -1,4 +1,35 @@
-const data = ['images/tadasana.png', 
+const data = [ // First 10 asanas
+              'images/tadasana.png', 
               'images/utkatasana.png', 
               'images/padangusthasana.png', 
-              'images/padahastasana.png']
+              'images/padahastasana.png',
+              'images/utthita-trikonasana.png', // Has right and left
+              'images/utthita-trikonasana.png', // Has right and left
+              'images/parivrtta-trikonasana.png', // Has right and left
+              'images/parivrtta-trikonasana.png', // Has right and left
+              'images/utthita-parsvakonasana.png', // Has right and left side
+              'images/utthita-parsvakonasana.png', // Has right and left side
+              'images/parivrtta-parsvakonasana.png', // Has right and left side
+              'images/parivrtta-parsvakonasana.png', // Has right and left side
+              'images/prasarita-padottanasana.png',
+              'images/parsvottanasana.png' // Has right and left side
+              'images/parsvottanasana.png' // Has right and left side
+              // 11 - 20
+              // Utthita Hasta Padangusthasana (Extended Hand to Big Toe Pose)
+              // Garudasana (Eagle Pose)
+              // Vrksasana (Tree Pose)
+              // Utthita Ashwa Sanchalanasana (High Lunge)
+              // Adho Mukha Svanasana (Downward-Facing Dog Pose)
+              // Anjaneyasana (Low Lunge)
+              // Urdhva Mukha Svanasana (Upward-Facing Dog Pose)
+              // Balasana (Child’s Pose)
+              // Bhujangasana (Cobra Pose)
+              // Salabhasana (Locust Pose)
+              // 21 - 30
+              // 31 - 40
+              // 41 - 50
+              // 51 - 60
+              // 61 - 70
+              // 71 - 80
+              // 81 - 84
+            ]
